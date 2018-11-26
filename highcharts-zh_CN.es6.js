f6de1b2e0d306b89efdd63aad91ab8361ac78876
jianshukeji <john@jianshukeji.com>
2018-11-26 20:04:28 +0800

    update some lang options

--- highcharts-zh_CN.es6.js
+++ highcharts-zh_CN.es6.js
@@ -1,5 +1,5 @@
 /**
- * Highcharts-zh_CN ES6 版本 plugins v1.1 (2018-07-31)
+ * Highcharts-zh_CN ES6 版本 plugins v1.1 (2018-11-26)
  *
  * (c) 2017 Jianshu Technology Co.,LTD (https://jianshukeji.com)
  *
@@ -10,17 +10,19 @@
 
 export default H => {
 
-    var protocol = window.location.protocol;
+    let protocol = window.location.protocol;
 
-    var defaultOptionsZhCn = {
+    let defaultOptionsZhCn = {
         lang: {
             // Highcharts
             contextButtonTitle: '图表导出菜单',
             decimalPoint: '.',
-            downloadJPEG: "下载JPEG图片",
-            downloadPDF: "下载PDF文件",
-            downloadPNG: "下载PNG文件",
-            downloadSVG: "下载SVG文件",
+            downloadJPEG: "下载 JPEG 图片",
+            downloadPDF: "下载 PDF 文件",
+            downloadPNG: "下载 PNG 文件",
+            downloadSVG: "下载 SVG 文件",
+            downloadXLS: '下载 XLS 文件',
+            openInCloud: '在 Highcharts Cloud 中打开',
             drillUpText: "返回 {series.name}",
             invalidDate: '无效的时间',
             loading: '加载中...',
@@ -33,7 +35,7 @@ export default H => {
             shortMonths: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
             thousandsSep: ',',
             weekdays: ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
-
+            viewData: '查看数据表格',
             // Highstock
             rangeSelectorFrom: '开始时间',
             rangeSelectorTo: '结束时间',
@@ -41,7 +43,7 @@ export default H => {
 
             // Highmaps
             zoomIn: '缩小',
-            zoomOut: '放大'
+            zoomOut: '放大',
         },
 
         global: {
--- highcharts-zh_CN.src.js
+++ highcharts-zh_CN.src.js
@@ -1,7 +1,7 @@
 /**
- * Highcharts-zh_CN plugins v1.0.3 (2017-04-05)
+ * Highcharts-zh_CN plugins v1.1.0 (2018-11-26)
  *
- * (c) 2017 Jianshu Technology Co.,LTD (https://jianshukeji.com)
+ * (c) 2018 Jianshu Technology Co.,LTD (https://jianshukeji.com)
  *
  * Author : john@jianshukeji.com, Blue Monkey
  *
@@ -23,10 +23,12 @@
             // Highcharts
             contextButtonTitle: '图表导出菜单',
             decimalPoint: '.',
-            downloadJPEG: "下载JPEG图片",
-            downloadPDF: "下载PDF文件",
-            downloadPNG: "下载PNG文件",
-            downloadSVG: "下载SVG文件",
+            downloadJPEG: "下载 JPEG 图片",
+            downloadPDF: "下载 PDF 文件",
+            downloadPNG: "下载 PNG 文件",
+            downloadSVG: "下载 SVG 文件",
+            downloadXLS: '下载 XLS 文件',
+            openInCloud: '在 Highcharts Cloud 中打开',
             drillUpText: "返回 {series.name}",
             invalidDate: '无效的时间',
             loading: '加载中...',
@@ -39,7 +41,7 @@
             shortMonths: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
             thousandsSep: ',',
             weekdays: ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
-
+            viewData: '查看数据表格',
             // Highstock
             rangeSelectorFrom: '开始时间',
             rangeSelectorTo: '结束时间',
@@ -47,7 +49,7 @@
 
             // Highmaps
             zoomIn: '缩小',
-            zoomOut: '放大'
+            zoomOut: '放大',
         },
 
         global: {
